01d0d7438ba09d37d94c0d5cbde7684413f9a657
GretchenGit <58035612+GretchenGit@users.noreply.github.com>
2019-11-28 09:50:12 +0100

    scopes and types exercise

---
+++ assets/js/scopres_and_types.js
@@ -0,0 +1,51 @@
+/** 
+ *
+ *  @desc Erste Übung Javascriptkurs
+ *
+ * @package Webapplication
+ * @module 
+ * @author Silke <silke.rappelt@autark.com>
+ * @version v1.0.0
+ * @since 2019-11-28
+ * @see i.e. inspired by ... {link to}
+ * @license MIT {https://opensource.org/licenses/MIT}
+ * @copyright (c) 2019 Silke Rappelt, Cassel
+ */
+
+!(function () {
+    'use strict';
+    // DECLARATION
+    let zahl = 42,
+        satz = 'Hallo Welt',
+        wahrheit = true,
+        meinArray = ['1', '2', '3', 1, 2, 3],
+        meinObject = {
+            objectkey1: 'Hallo Welt 2'
+        },
+        eineFunktion = function () {};
+
+    // FUNCTION
+    /**
+     * Die Funktion log schreibt den Variablennamen, den Typ und den Wert in die Konsole
+     * @param {*} elementname 
+     * @param {*} element 
+     */
+    function log(elementname, element) {
+        let ausgabe = elementname + ' { ' + typeof (element) + ' }: ' + element
+        console.log(ausgabe);
+    };
+
+
+    // CONTROL
+    log('zahl', zahl);
+    log('satz', satz);
+    log('wahrheit', wahrheit);
+    log('meinArray', meinArray);
+    log('meinObject', meinObject.objectkey1);
+    log('eineFunktion', eineFunktion);
+
+    // Zugriff aus globalen Kontext ermöglichen
+    window.Autark = {} || window.Autark;
+    window.Autark.log = log();
+
+})()
